980f98e313cd3c2dc6c4d6ef4cb80e447370833a
Geenod Saniru <77047196+GeenodSaniru@users.noreply.github.com>
2025-11-08 13:58:55 +0530

    final tests and corrections

--- src/main/java/ac/nsbm/onvent/exception/InsufficientSeatsException.java
+++
@@ -1,7 +0,0 @@
-package ac.nsbm.onvent.exception;
-
-public class InsufficientSeatsException extends RuntimeException {
-    public InsufficientSeatsException(String message) {
-        super(message);
-    }
-}
